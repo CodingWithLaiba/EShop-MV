bdbfa1c13644de4052b3b7d80020e5230ec7f26b
Laiba Ijaz <laibaijaz904@gmail.com>
2026-08-28 15:13:36 +0500

    Implement signup functionality with API integration and toast notifications

--- Frontend/src/components/Signup/Signup.jsx
+++ Frontend/src/components/Signup/Signup.jsx
@@ -3,6 +3,9 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
 import { RxAvatar } from "react-icons/rx";
+import axios from "axios";
+import { server } from "../../server";
+import { toast } from "react-toastify";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
@@ -15,9 +18,22 @@ export default function Signup() {
     const file = e.target.files[0];
     setAvatar(file);
   };
-  //  const handleSubmit=()=>{
-  //     console.log("submit")
-  //  }
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    axios
+      .post(`${server}/user/create-user`, { name, email, password })
+      .then((res) => {
+        toast.success(res.data.message);
+        setName("");
+        setEmail("");
+        setPassword("");
+        setAvatar();
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      });
+  };
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -27,7 +43,7 @@ export default function Signup() {
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6">
+          <form className="space-y-6"  onSubmit={handleSubmit}>
             <div>
               <label
                 htmlFor="name"
