ca1cb0a87f1c9a27760ce5a9f9271ce5a535f944
Laiba Ijaz <laibaijaz904@gmail.com>
2026-08-28 15:48:14 +0500

    Add sendMail utility and implement email activation in user creation

--- Backend/controller/user.js
+++ Backend/controller/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const User = require("../model/user");
 const ErrorHandler = require("../utils/ErrorHandler");
+const sendMail = require("../utils/sendMail");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -19,46 +21,43 @@ router.post("/create-user", async (req, res, next) => {
     //   folder: "avatars",
     // });
 
-    const user = await User.create({
-      name,
-      email,
-      password,
+    const user = {
+      name: name,
+      email: email,
+      password: password,
       // avatar: {
       //   public_id: myCloud.public_id,
       //   url: myCloud.secure_url,
       // },
-    });
+    };
 
-    res.status(201).json({
-      success: true,
-      user,
-    });
+    const activationToken = createActivationToken(user);
+    const activationUrl = `http://localhost:5173/activation/${activationToken}`;
+
+    try {
+      await sendMail({
+        email: user.email,
+        subject: "Activate your account",
+        message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
+      });
+
+      res.status(201).json({
+        success: true,
+        message: `please check your email:- ${user.email} to activate your account!`,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
   } catch (error) {
     return next(new ErrorHandler(error.message, 400));
   }
 });
 
-
-// const activationToken = createActivationToken(user);
-
-// const activationUrl = `https://eshop-tutorial-pyri.vercel.app/activation/${activationToken}`;
-
-//     try {
-//       await sendMail({
-//         email: user.email,
-//         subject: "Activate your account",
-//         message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
-//       });
-//       res.status(201).json({
-//         success: true,
-//         message: `please check your email:- ${user.email} to activate your account!`,
-//       });
-//     } catch (error) {
-//       return next(new ErrorHandler(error.message, 500));
-//     }
-//   } catch (error) {
-//     return next(new ErrorHandler(error.message, 400));
-//   }
-// });
+// create activation token
+const createActivationToken = (user) => {
+  return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+    expiresIn: "5m",
+  });
+};
 
 module.exports = router;
