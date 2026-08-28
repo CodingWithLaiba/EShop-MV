fbf745044722db96e9156500b73c125304ce6d30
Laiba Ijaz <laibaijaz904@gmail.com>
2026-08-28 15:12:15 +0500

    Initialize Express server with middleware and user routes

---
+++ Backend/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const ErrorMiddleware = require("./middelware/error");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static("uploads"));
+app.use("/test", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "config/.env",
+  });
+}
+
+// import routes
+const user = require("./controller/user");
+
+app.use("/api/v2/user", user);
+
+//for errorhandler
+app.use(ErrorMiddleware);
+module.exports = app;
