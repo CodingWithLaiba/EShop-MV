6ad07bfe089fc0c295462e79052d98ee54b7ac4e
Laiba Ijaz <laibaijaz904@gmail.com>
2026-08-29 17:07:20 +0500

    Implement user activation and login functionality with JWT and error handling

--- Backend/controller/user.js
+++ Backend/controller/user.js
@@ -2,8 +2,10 @@ const express = require("express");
 const User = require("../model/user");
 const ErrorHandler = require("../utils/ErrorHandler");
 const sendMail = require("../utils/sendMail");
+const catchAsyncErrors = require("../middelware/catchAsyncErrors");
 const jwt = require("jsonwebtoken");
-
+const sendToken = require("../utils/jwtToken");
+const { isAuthenticated } = require("../middelware/auth");
 const router = express.Router();
 
 // create user
@@ -56,8 +58,123 @@ router.post("/create-user", async (req, res, next) => {
 // create activation token
 const createActivationToken = (user) => {
   return jwt.sign(user, process.env.ACTIVATION_SECRET, {
-    expiresIn: "5m",
+    expiresIn: "30m",
   });
 };
+
+// activate user
+router.post(
+  "/activation",
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const { activation_token } = req.body;
+
+      if (!activation_token) {
+        return next(new ErrorHandler("Activation token is required", 400));
+      }
+
+      const newUser = jwt.verify(
+        activation_token,
+        process.env.ACTIVATION_SECRET,
+      );
+
+      const { name, email, password, avatar } = newUser;
+
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return next(new ErrorHandler("User already exists", 400));
+      }
+
+      user = await User.create({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      sendToken(user, 201, res);
+    } catch (error) {
+      console.log("ACTIVATION ERROR:", error);
+
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }),
+);
+
+// login user
+router.post(
+  "/login-user",
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return next(new ErrorHandler("Please provide the all fields!", 400));
+      }
+
+      const user = await User.findOne({ email }).select("+password");
+
+      if (!user) {
+        return next(new ErrorHandler("User doesn't exists!", 400));
+      }
+
+      const isPasswordValid = await user.comparePassword(password);
+
+      if (!isPasswordValid) {
+        return next(
+          new ErrorHandler("Please provide the correct information", 400)
+        );
+      }
+
+      sendToken(user, 201, res);
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  })
+);
+
+// load user
+router.get(
+  "/getuser",
+  isAuthenticated,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const user = await User.findById(req.user.id);
+
+      if (!user) {
+        return next(new ErrorHandler("User doesn't exists", 400));
+      }
+
+      res.status(200).json({
+        success: true,
+        user,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  })
+);
+
+// log out user
+// router.get(
+//   "/logout",
+//   catchAsyncErrors(async (req, res, next) => {
+//     try {
+//       res.cookie("token", null, {
+//         expires: new Date(Date.now()),
+//         httpOnly: true,
+//         sameSite: "none",
+//         secure: true,
+//       });
+//       res.status(201).json({
+//         success: true,
+//         message: "Log out successful!",
+//       });
+//     } catch (error) {
+//       return next(new ErrorHandler(error.message, 500));
+//     }
+//   })
+// );
 
 module.exports = router;
