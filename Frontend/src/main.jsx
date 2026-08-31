8b5db1f8a720105a01616f686230c60724c0ee05
Laiba Ijaz <laibaijaz904@gmail.com>
2026-08-31 09:29:52 +0500

    Refactor main.jsx to use createRoot for rendering and clean up imports

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,13 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-import './index.css'
-import ReactDOM from "react-dom";
-import App from './App.jsx'
-import {Provider} from "react-redux"
-import Store from "./redux/store.js"
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+import "./index.css";
+
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import Store from "./redux/store.js";
+
+createRoot(document.getElementById("root")).render(
   <Provider store={Store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
